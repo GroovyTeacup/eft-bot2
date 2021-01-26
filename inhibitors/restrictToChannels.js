d544388a972adf33d290e69d0549123ad557c8ca
salami <salami@win.local>
2021-01-26 02:51:06 -0500

    Add channel restriction command whitelist

--- inhibitors/restrictToChannels.js
+++ inhibitors/restrictToChannels.js
@@ -1,11 +1,17 @@
 const { Inhibitor } = require('discord-akairo');
 const EFTMessage = require('../eftMessage');
+const ContentParser = require('discord-akairo/src/struct/commands/ContentParser');
 
 class RestrictChannels extends Inhibitor {
     constructor() {
         super('blockIdiotCheck', {
             reason: 'blockIdiotCheck'
         })
+
+        this.commandWhitelist = [
+            "mute",
+            "unmute"
+        ]
     }
 
     /**
@@ -16,7 +22,7 @@ class RestrictChannels extends Inhibitor {
      * @memberof RestrictChannels
      */
     exec(message) {
-        if (message.member.hasPermission("ADMINISTRATOR")) return
+        if (message.member.hasPermission("ADMINISTRATOR")) return // Administrators can run commands wherever they want
 
         let configServer = this.client.configServer
         let prefix = this.client.config.CommandPrefix
@@ -24,6 +30,20 @@ class RestrictChannels extends Inhibitor {
         validChannels.push(configServer.IdiotCheck)
         validChannels.push(configServer.CommandSpam)
         validChannels.push(configServer.CommandSpamStaff)
+
+        // Allow non-administrator staff members to use certain whitelisted commands outside of the command spam channels
+        if (message.content.startsWith(prefix) && message.client.isStaffMember(message.member))
+        {
+            let parser = new ContentParser()
+
+            let arr = parser.parse(message.content).all
+            let command = arr[0].value
+            command = command.substr(1, command.length)
+            if (this.commandWhitelist.includes(command))
+            {
+                return false;
+            }
+        }
 
         if (validChannels.includes(message.channel.id))
         {
