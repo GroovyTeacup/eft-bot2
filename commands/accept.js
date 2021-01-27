c02272defa9f69b22375065e8b63402270520437
salami <salami@win.local>
2021-01-26 20:29:01 -0500

    Check for member before accept

--- commands/accept.js
+++ commands/accept.js
@@ -43,6 +43,10 @@ class AcceptCommand extends Command {
         await member.roles.add(memberRoleId).catch((reason) => console.error(`Failed to add member role to member ${member.id} (${reason})`))
 
         let dbHandler = message.client.databaseHandler
+
+        let dbMember = await dbHandler.getMemberById(member.id)
+        if (dbMember != null) return // don't register members already in the database
+
         await dbHandler.addMember(member)
         console.log(`Registered ${member.id} as a new member. (!accept)`)
     }
